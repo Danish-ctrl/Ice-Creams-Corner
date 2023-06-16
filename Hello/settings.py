987e9b5bf4c5bf44b839833fe2ecc35e876d3eff
Danish <danish.reza1@gmail.com>
2023-06-16 03:52:34 +0200

    added files for heroku

--- Hello/settings.py
+++ Hello/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import os
+import dj_database_url
 from django.contrib.messages import constants as messages
 
 
@@ -27,7 +30,7 @@ SECRET_KEY = 'django-insecure-=c(o9ef=@u%+sm!kgmd_##$1i%5kd$1dkyjzpa6f@&62eob^5f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,3 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+django_heroku.settings(locals())
